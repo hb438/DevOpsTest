3f742851a06b1849ea8517ecbd0d293ec486597b
Harsh-Admin <harsh-admin@Krishs-MacBook-Air.local>
2019-07-09 21:12:08 +0100

    updated READme

---
+++ Infra/task-definition.tf
@@ -0,0 +1,93 @@
+data "aws_ecs_task_definition" "Devopstest" {
+  task_definition = "${aws_ecs_task_definition.Devopstest.family}"
+}
+
+resource "aws_ecs_task_definition" "Devopstest" {
+    family                = "php-app"
+    container_definitions = <<DEFINITION
+[
+  {
+    "name": "dev-test-nginx",
+    "links": [
+      "php-fpm"
+    ],
+    "volumes": [
+      "./application:/var/www/html",
+      "./nginx/logs/:/var/log/nginx",
+      "./nginx/sites/:/etc/nginx/conf.d"
+    ],
+    "image": "dev-test-nginx",
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ],
+    "memory": 500,
+    "cpu": 10
+  },
+
+  {
+    "name": "php-fpm",
+    "volumes": [
+      "./application:/var/www/html"
+    ],
+    "image": "php-fpm",
+    "environment": [
+      "env/${ENV}.env"
+    ]
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 9000,
+        "hostPort": 80
+      }
+    ],
+    "memory": 500,
+    "cpu": 10
+  },
+
+{
+    "name": "dev-test-postgresql",
+    "volumes": [
+      "./postgresql/data/:/var/lib/postgresql/data"
+    ],
+    "image": "dev-test-postgresql",
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 5432,
+        "hostPort": 5432
+      }
+    ],
+    "memory": 500,
+    "cpu": 10
+  },
+  {
+    "environment": [
+      "POSTGRES_DB=default,
+      "POSTGRES_USER=default",
+      "POSTGRES_PASSWORD=secret"
+    ],
+{
+    "name": "dev-test-redis",
+    "volumes": [
+      "./redis/data/:/var/lib/redis/data"
+    ],
+    "image": "dev-test-redi",
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 6379,
+        "hostPort": 6379
+      }
+    ],
+    "memory": 500,
+    "cpu": 10
+  },
+
+  }
+]
+DEFINITION
+}
--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # DevOpsTest
+
+
+The php-fpm app is deployed in AWS ECS cluster and is scalable and immutable.
+
+Initial steps is to push the docker images of nginx, php-fpm, postgres and redis onto the AWS ECR repository and the applcation is deployed in ECS cluster using the task definition.
+
+docker login - pass the cred for aws ecr repo
+
+docker build <name of container i.e nginx,php-fpm,postgres,redis> .
+
+docker push <name of container i.e nginx,php-fpm,postgres,redis>: latest
+
+Infra Configuration:
+
+AWS credentials are exported locally to deploy the infra.
+
+The infra consists of ALB, ASG, ECS Cluster, VPC and is deployed using terraform templates.
+
+terraform init - To initialise the infra
+
+terraform plan
+
+terraform apply
+
+Alternate way to deploy:
+
+The app can also be deployed in K8's cluster where individual services will be deloyed in pods and exposed using services and Ingress controller.
+
+There can also be CI pipeline that can be implemented using jenkins where a new version of app can be pushed to aws ECR repo and then new pods with replcia sets is deployed/updated.
+
+The security aspects can be tightend by deploying app wihtin the private subnet and passing traffic through the ALB.
+
+Pending bit is assigning the DNS to a relevent container.
